7cb40e3e7e2f49b5e1a9984eb43231c28593bd91
Dasol Baek <schgana@hotmail.com>
2018-12-06 11:34:46 +1300

    migration and seed for logs table

--- seeds/logs.js
+++ seeds/logs.js
@@ -1,13 +1,9 @@
-
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+exports.seed = knex =>
+  knex('logs').del()
+    .then(() =>
+      knex('logs').insert([
+        {id: 1, user_id: 1},
+        {id: 2, user_id: 1},
+        {id: 3, user_id: 1}
+      ])
+    )
