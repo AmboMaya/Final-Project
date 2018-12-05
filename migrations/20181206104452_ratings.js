ec84c1001c832f04b7a155409ca1f2ddb9be5236
Dasol Baek <schgana@hotmail.com>
2018-12-06 12:02:16 +1300

    ratings migration and seed

--- migrations/20181206104452_ratings.js
+++ migrations/20181206104452_ratings.js
@@ -1,8 +1,10 @@
+exports.up = knex =>
+  knex.schema.createTable('ratings', t => {
+    t.increments('id').primary()
+    t.integer('log_id').unique()
+    t.integer('activity_id').unique()
+    t.integer('rating')
+    t.string('detail')
+  })
 
-exports.up = function(knex, Promise) {
-  
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = knex => knex.schema.dropTable('ratings')
