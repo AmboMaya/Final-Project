5c326682505cc682bb8f77a049646f55611dd7df
alliewebb <alliewebb@Allies-MacBook-Pro.local>
2018-12-07 10:34:44 +1300

    changed activities column to log. added more ids to dates seed data

--- seeds/dates.js
+++ seeds/dates.js
@@ -4,6 +4,10 @@ exports.seed = knex =>
       knex('dates').insert([
         {id: 1, user_id: 1},
         {id: 2, user_id: 1},
-        {id: 3, user_id: 1}
+        {id: 3, user_id: 1},
+        {id: 4, user_id: 1},
+        {id: 5, user_id: 1},
+        {id: 6, user_id: 1},
+        {id: 7, user_id: 1}
       ])
     )
