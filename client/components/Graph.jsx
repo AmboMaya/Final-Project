eecdfce126077b68690e6df92d8271b22379625b
alliewebb <alliewebb@Allies-MacBook-Pro.local>
2018-12-06 15:08:58 +1300

    took out unnecessary comments, added comments to indicate placeholders

--- client/components/Graph.jsx
+++ client/components/Graph.jsx
@@ -11,56 +11,56 @@ export default class Chart extends Component {
                 borderColor: 'red',
                 backgroundColor: 'red',
                 label: 'Mood',
-                data: [1, 3, 3, 5, 2, 1],
+                data: [1, 3, 3, 5, 2, 1], //Placeholders for data
                 borderWidth: 2
             },{
                 fill: false,
                 borderColor: 'rgba(75,192,192,1)',
                 backgroundColor: 'rgba(75,192,192,1)',
                 label: 'Diet',
-                data: [2, 4, 1, 5, 1, 3],
+                data: [2, 4, 1, 5, 1, 3], //Placeholders for data
                 borderWidth: 2
             },{
                 fill: false,
                 borderColor: 'blue',
                 backgroundColor: 'blue',
                 label: 'Exercise',
-                data: [2, 2, 1, 2, 2, 3],
+                data: [2, 2, 1, 2, 2, 3], //Placeholders for data
                 borderWidth: 2
             },{
                 fill: false,
                 borderColor: 'pink',
                 backgroundColor: 'pink',
                 label: 'Sleep',
-                data: [5, 4, 3, 5, 2, 3],
+                data: [5, 4, 3, 5, 2, 3], //Placeholders for data
                 borderWidth: 2
             },{
                 fill: false,
                 borderColor: 'yellow',
                 backgroundColor: 'yellow',
                 label: 'Meditation',
-                data: [2, 3, 5, 1, 1, 2],
+                data: [2, 3, 5, 1, 1, 2], //Placeholders for data
                 borderWidth: 2
             },{
                 fill: false,
                 borderColor: 'purple',
                 backgroundColor: 'purple',
                 label: 'Alcohol',
-                data: [5, 4, 3, 2, 1, 5],
+                data: [5, 4, 3, 2, 1, 5], //Placeholders for data
                 borderWidth: 2
             },{
                 fill: false,
                 borderColor: 'orange',
                 backgroundColor: 'orange',
                 label: 'Water',
-                data: [1, 2, 1, 5, 4, 4],
+                data: [1, 2, 1, 5, 4, 4], //Placeholders for data
                 borderWidth: 2
             },{
                 fill: false,
                 borderColor: 'green',
                 backgroundColor: 'green',
                 label: 'Vice',
-                data: [2, 5, 1, 3, 2, 1],
+                data: [2, 5, 1, 3, 2, 1], //Placeholders for data
                 borderWidth: 2
             }
         ]
@@ -76,9 +76,7 @@ export default class Chart extends Component {
                 yAxes: [{
                     ticks: {
                         callback: function(label, index, labels) {
-                            // for a value (tick) equals to 8
                             return yLabels[label];
-                            // 'junior-dev' will be returned instead and displayed on your chart
                         }
                     }
                 }]
