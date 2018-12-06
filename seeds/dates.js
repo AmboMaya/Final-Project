c1508b975c6be4fc32fe062c92c46132ae6a0af0
alliewebb <alliewebb@Allies-MacBook-Pro.local>
2018-12-07 10:46:12 +1300

    added dates and times to dates seed

--- seeds/dates.js
+++ seeds/dates.js
@@ -2,12 +2,12 @@ exports.seed = knex =>
   knex('dates').del()
     .then(() =>
       knex('dates').insert([
-        {id: 1, user_id: 1},
-        {id: 2, user_id: 1},
-        {id: 3, user_id: 1},
-        {id: 4, user_id: 1},
-        {id: 5, user_id: 1},
-        {id: 6, user_id: 1},
-        {id: 7, user_id: 1}
+        {id: 1, user_id: 1, created_at: '2018-12-06 21:35:55'},
+        {id: 2, user_id: 1, created_at: '2018-12-06 21:35:55'},
+        {id: 3, user_id: 1, created_at: '2018-12-06 21:35:55'},
+        {id: 4, user_id: 1, created_at: '2018-12-06 21:35:55'},
+        {id: 5, user_id: 1, created_at: '2018-12-06 21:35:55'},
+        {id: 6, user_id: 1, created_at: '2018-12-06 21:35:55'},
+        {id: 7, user_id: 1, created_at: '2018-12-06 21:35:55'}
       ])
     )
