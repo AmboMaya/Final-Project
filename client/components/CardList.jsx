6d1b3aebf53b5b71e1068a2cd12cc47b760b48f2
Chris Laupama <chrislaupama@icloud.com>
2018-12-06 21:23:50 +1300

    created state and mapped through state for activity card

--- client/components/CardList.jsx
+++ client/components/CardList.jsx
@@ -1,37 +1,36 @@
-import React from 'react'
-import {Card, Container, List, Grid} from 'semantic-ui-react'
-import ActivityCard from './ActivityCard'
+import React from "react"
+import { Container, Grid } from "semantic-ui-react"
+import ActivityCard from "./ActivityCard"
 
-const activityData = [
-  {name: 'mood', log: 'happy'},
-  {name: 'exercise', log: 'happy'},
-  {name: 'diet', log: 'happy'},
-  {name: 'sleep', log: 'happy'},
-  {name: 'meditation', log: 'happy'},
-  {name: 'water', log: 'happy'},
-  {name: 'alcohol', log: ' not happy'},
-  {name: 'vice', log: 'sad'}
-]
+export default class CardList extends React.Component {
+  state = {
+    activities: [
+      { name: "mood", log: "happy" },
+      { name: "exercise", log: "happy" },
+      { name: "diet", log: "happy" },
+      { name: "sleep", log: "happy" },
+      { name: "meditation", log: "happy" },
+      { name: "water", log: "happy" },
+      { name: "alcohol", log: " not happy" },
+      { name: "vice", log: "sad" }
+    ]
+  }
 
-function CardList (props) {
-  return (
-    <React.Fragment>
-      <Container>
-        <Grid columns={2} doubling stackable>
-          <Grid.Column align='center'>
-            <List>
-              <Card>
-                <h1>kia oraaaa</h1>
-                {/* {activityData.map(item => {
-                  <p>{item.name}</p>
-                  // <ActivityCard activity={item} />
-                })} */}
-              </Card>
-            </List>
-          </Grid.Column>
-        </Grid>
-      </Container>
-    </React.Fragment>
-  )
+  render() {
+    return (
+      <React.Fragment>
+        <Container>
+          <Grid columns={2} doubling stackable>
+            <Grid.Column align="center">
+              <div>
+                {this.state.activities.map(act => {
+                  return <ActivityCard name={act.name} log={act.log} />;
+                })}
+              </div>
+            </Grid.Column>
+          </Grid>
+        </Container>
+      </React.Fragment>
+    )
+  }
 }
-export default CardList
