4a8a73c8ee6944c82fc68092626f2ef11a9bd0a3
Alina Kireeva <alina.kireeva88@gmail.com>
2018-12-07 15:02:30 +1300

    changed activity card to map through smilies

--- client/components/ActivityCard.jsx
+++ client/components/ActivityCard.jsx
@@ -1,33 +1,52 @@
-import React from "react";
-import { Card, Button, Grid } from 'semantic-ui-react'
-import Smiley from "./Rating/Smiley"
+import React from 'react'
+import { Card, Button, Grid, Icon } from 'semantic-ui-react'
 
-const ActivityCard = props => {
-  
-  let smilies = []
-  for (let i = 1; i <= 5; i++) {
-    smilies.push(<Smiley rating={i} selected={props.currentRating === i} activity={props.activity} />)
+export default class ActivityCard extends React.Component {
+  state = {
+    smiles: [
+      {
+        mood: 'fa-angry'
+      },
+      {
+        mood: 'fa-frown'
+      },
+      {
+        mood: 'fa-meh'
+      },
+      {
+        mood: 'fa-smile'
+      },
+      {
+        mood: 'fa-laugh'
+      }
+    ]
   }
-  return (
-    <React.Fragment>
-      <Grid.Column align="center">
-        <Card>
-          <Card.Content>
-            <Grid>
-              <Grid.Column floated='right' width={5}>
-                <i className="ellipsis horizontal icon right" floated="right"></i>
-              </Grid.Column>
-            </Grid>
-            <Card.Header>{props.name}</Card.Header>
-            {smilies}
-          </Card.Content>
-          <Card.Content extra>
-            <i className="plus icon"></i>
-          </Card.Content>
-        </Card>
-      </Grid.Column>
-    </React.Fragment>
-  )
-}
 
-export default ActivityCard
+  render() {
+    return (
+      <React.Fragment>
+        <Grid.Column align='center'>
+          <Card>
+            <Card.Content>
+              <Grid>
+                <Grid.Column floated='right' width={5}>
+                  <i
+                    className='ellipsis horizontal icon right'
+                    floated='right'
+                  />
+                </Grid.Column>
+              </Grid>
+              <Card.Header>{this.props.name}</Card.Header>
+              {this.state.smiles.map(smile => {
+                return ( <i className={smile.mood}></i> )
+              })}
+            </Card.Content>
+            <Card.Content extra>
+              <i className='plus icon' />
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+      </React.Fragment>
+    )
+  }
+}
