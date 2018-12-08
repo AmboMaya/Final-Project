04a646169867b6a37576cf012b45506675992da3
Chris Laupama <chrislaupama@icloud.com>
2018-12-09 12:58:41 +1300

    changed activity card to take rating changes

--- client/components/ActivityCard.jsx
+++ client/components/ActivityCard.jsx
@@ -1,32 +1,32 @@
-import React from "react";
-import { Card, Grid } from "semantic-ui-react";
-import ActivityLog from "./ActivityLog";
+import React from 'react'
+import { Card, Grid } from 'semantic-ui-react'
+import ActivityLog from './ActivityLog'
 
 export default class ActivityCard extends React.Component {
   state = {
-    activity_id: "",
-    rating: "",
-    log: ""
-  };
+    activity_id: '',
+    rating: '',
+    log: ''
+  }
 
   handleChange = e => {
     this.setState({
       [e.target.name]: e.target.value
-    });
-  };
+    })
+  }
 
   submitHandler = e => {
-    e.preventDefault();
-    this.props.addRecord(this.props.id, this.state);
+    e.preventDefault()
+    this.props.addRecord(this.props.id, this.state)
     // Final validation before submitting to server
-    console.log("SUBMITTED:", this.state);
+    console.log('SUBMITTED:', this.state)
     // Clear the state to clear the form
     this.setState({
-      activity_id: "",
-      rating: "",
-      log: ""
-    });
-  };
+      activity_id: '',
+      rating: '',
+      log: ''
+    })
+  }
 
   render() {
     return (
@@ -43,19 +43,17 @@ export default class ActivityCard extends React.Component {
                 </Grid.Column>
               </Grid>
 
-              <Card.Header>
-                {this.props.name}
-              </Card.Header>
+              <Card.Header>{this.props.name}</Card.Header>
               <Grid.Column align="center">
                 {this.props.smiles.map(smile => {
                   return (
                     <i
-                      className={"far " + `${smile.mood}` + " fa-3x facesInCss"}
+                      className={'far ' + `${smile.mood}` + ' fa-3x facesInCss'}
                       key={smile.value}
                       type="submit"
                       onSubmit={this.submitHandler}
                     />
-                  );
+                  )
                 })}
               </Grid.Column>
             </Card.Content>
@@ -65,6 +63,6 @@ export default class ActivityCard extends React.Component {
           </Card>
         </Grid.Column>
       </React.Fragment>
-    );
+    )
   }
 }
