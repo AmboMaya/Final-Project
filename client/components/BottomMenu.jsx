91dd45cb2e966e5ad3a2de9c7700404e67b759d8
kiri-tavaga <kiri.tavaga@icloud.com>
2018-12-09 12:23:26 +1300

    onClick takes menubutton to paths

--- client/components/BottomMenu.jsx
+++ client/components/BottomMenu.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const BottomMenu = () => {
-  return (
-    <div className="ui teal three item inverted menu">
-      <a className="active item" path='/'>
+export default class BottomMenu extends Component {
+  handleClick (e) {
+    e.preventDefault()
+  }
+
+  render () {
+    return (
+      <div className="ui teal three item inverted menu">
+        <a href='/' onClick={this.handleClick} className="active item">
     Home
-      </a>
-      <a className="item">
-    Messages
-      </a>
-      <a className="item">
-    Friends
-      </a>
-    </div>
-  )
+        </a>
+        <a className="item">
+    Calendar
+        </a>
+        <a href='/records' onClick={this.handleClick} className="item">
+    Graph
+        </a>
+      </div>
+    )
+  }
 }
-
-export default BottomMenu
