ca065f4292ddcd49fd1976733ef43022d413c298
alliewebb <alliewebb@Allies-MacBook-Pro.local>
2018-12-10 11:54:50 +1300

    changed migration and seed data to include date only

--- migrations/20181206162707_dates.js
+++ migrations/20181206162707_dates.js
@@ -2,7 +2,7 @@ exports.up = knex =>
   knex.schema.createTable('dates', t => {
     t.increments('id').primary()
     t.integer('user_id').references('users.id')
-    t.timestamps(true, true)
+    t.date('created_at')
   })
 
 exports.down = knex => knex.schema.dropTable('dates')
