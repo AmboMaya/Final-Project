a138fef9a4fe1b888d7e859bf8c19bafdf77c697
Chris Laupama <chrislaupama@icloud.com>
2018-12-09 14:03:29 +1300

    changed submit handle to be props

--- client/components/BottomMenu.jsx
+++ client/components/BottomMenu.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const BottomMenu = () => {
+const BottomMenu = (props) => {
   return (
     <div className="ui teal three item inverted menu">
       <a className="active item" path='/'>
     Home
       </a>
-      <a className="item" onClick={this.handleSubmit} type='submit'>
+      <a className="item" onClick={props.handleSubmit} type='submit'>
     Submit
       </a>
       <a className="item">
