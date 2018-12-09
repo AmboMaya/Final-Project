72aa23075a5efdaca26c87c4be2c66f6c1d762f4
CarelS072 <carelstarreveld@hotmail.com>
2018-12-09 14:29:31 +1300

    added heroku post-install script

--- knexfile.js
+++ knexfile.js
@@ -30,7 +30,7 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './migrations'
+      tableName: 'knex_migrations'
     },
     useNullAsDefault: true
   }
