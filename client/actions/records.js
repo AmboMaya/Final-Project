330ccc4b1c1e728c8601510c626825234163d6ad
Chris Laupama <chrislaupama@icloud.com>
2018-12-10 14:29:52 +1300

    formatted recuder to handle activities and logs

--- client/actions/records.js
+++ client/actions/records.js
@@ -1,9 +1,9 @@
 import request from 'superagent'
 
-export function addNewRecordSuccess(newRecord) {
+export function addRecordSuccess(record) {
   return {
     type: 'ADD_RECORD_SUCCESS',
-    newRecord
+    record
   }
 }
 
@@ -20,14 +20,27 @@ export function getRecordError(message) {
   }
 }
 
-export function addNewRecord(id, newRecord) {
+export function addActivity(id, record) {
   return dispatch => {
     dispatch(getRecordPending())
     return request
       .post('/api/v1/records')
-      .send({ userId: id, records: [ newRecord ]})
+      .send({ userId: id, records: [ record ]})
       .then(res => {
         dispatch(addNewRecordSuccess(res.body))
+      })
+      .catch(err => dispatch(getRecordError(err.message)))
+  }
+}
+
+export function addLog(id, record) {
+  return dispatch => {
+    dispatch(getRecordPending())
+    return request
+      .post('/api/v1/records') // do we still want it to go to the same api?
+      .send({ userId: id, records: [ record ]})
+      .then(res => {
+        dispatch(addRecordSuccess(res.body))
       })
       .catch(err => dispatch(getRecordError(err.message)))
   }
