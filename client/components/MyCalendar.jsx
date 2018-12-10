248af55b9fef78ff949942cc00da8e27e746dedd
kiri-tavaga <kiri.tavaga@icloud.com>
2018-12-10 18:46:03 +1300

    link loading comp to pending reqs

--- client/components/MyCalendar.jsx
+++ client/components/MyCalendar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Calendar from 'react-input-calendar'
 import BottomMenu from './BottomMenu'
+import Loading from './loading'
 
 class MyCalendar extends React.Component {
   render () {
+    if (this.props.pending) {
+      return <Loading />
+    }
     return (
       <div>
         <Calendar format='DD/MM/YYYY' date='04-11-2018' />
