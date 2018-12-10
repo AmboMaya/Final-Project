da5971405e9ac2708daf4ffaef816ae4b76adb75
kiri-tavaga <kiri.tavaga@icloud.com>
2018-12-10 15:45:58 +1300

    changed statistics path and set menu to bottom for calendar

--- client/components/BottomMenu.jsx
+++ client/components/BottomMenu.jsx
@@ -1,19 +1,17 @@
 import React, {Component} from 'react'
-import {Link, BrowserRouter as Router} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import {Container, Menu} from 'semantic-ui-react'
 
 export default class BottomMenu extends Component {
   render () {
     return (
-      <Router>
-        <Menu borderless className='ui teal three item inverted menu' color='green' fixed='bottom' inverted>
-          <Container>
-            <Menu.Item as={Link} to='/' className='active item'>Home</Menu.Item>
-            <Menu.Item as={Link} to='/calendar' className='item'>Calendar</Menu.Item>
-            <Menu.Item as={Link} to='/records' className='item'>Graph</Menu.Item>
-          </Container>
-        </Menu>
-      </Router>
+      <Menu borderless className='ui teal three item inverted menu' color='green' fixed='bottom' inverted>
+        <Container>
+          <Menu.Item as={Link} to='/' className='active item'>Home</Menu.Item>
+          <Menu.Item as={Link} to='/calendar' className='item'>Calendar</Menu.Item>
+          <Menu.Item as={Link} to='/statistics' className='item'>Graph</Menu.Item>
+        </Container>
+      </Menu>
     )
   }
 }
