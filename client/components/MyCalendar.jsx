1b42dba698659f501bf860df18b918a73509279e
kiri-tavaga <kiri.tavaga@icloud.com>
2018-12-11 09:59:01 +1300

    date selected working

--- client/components/MyCalendar.jsx
+++ client/components/MyCalendar.jsx
@@ -2,15 +2,29 @@ import React from 'react'
 import Calendar from 'react-input-calendar'
 import BottomMenu from './BottomMenu'
 import Loading from './Loading'
+import moment from "moment"
 
 class MyCalendar extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      selectedDate:moment(),
+    }
+  }
+
+  onSelect = (e) => {
+    this.setState({selectedDate:moment(e)})
+  }
+
   render () {
     if (this.props.pending) {
       return <Loading />
     }
+
     return (
       <div>
-        <Calendar format='DD/MM/YYYY' />
+        <p> The date you've selected is: {this.state.selectedDate.format('YYYY-MM-DD')} </p>
+        <Calendar date={this.state.selectedDate} onChange={this.onSelect} />
         <BottomMenu />
       </div>
     )
