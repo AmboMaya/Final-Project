cce5c5a8f249ba71f6d689e526d4a398fac1a7c6
kiri-tavaga <kiri.tavaga@icloud.com>
2018-12-11 16:04:06 +1300

    Added photo of the day feature to replace calendar

--- client/components/MyCalendar.jsx
+++ client/components/MyCalendar.jsx
@@ -3,6 +3,8 @@ import Calendar from 'react-input-calendar'
 import BottomMenu from './BottomMenu'
 import Loading from './Loading'
 import moment from "moment"
+import Photo from './Photo'
+import { Placeholder } from '../../node_modules/semantic-ui-react';
 
 class MyCalendar extends React.Component {
   constructor(){
@@ -23,8 +25,10 @@ class MyCalendar extends React.Component {
 
     return (
       <div>
-        <p> The date you've selected is: {this.state.selectedDate.format('YYYY-MM-DD')} </p>
+        <Placeholder> Today is: {this.state.selectedDate.format('YYYY-MM-DD')} </Placeholder>
         <Calendar date={this.state.selectedDate} onChange={this.onSelect} />
+        <Photo />
+        <Placeholder>Capture your mood with a photo of the day </Placeholder>
         <BottomMenu />
       </div>
     )
