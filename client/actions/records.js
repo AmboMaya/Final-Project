60ed3d4eebbf4a6e42cb93f153b1508366cd0356
Chris Laupama <chrislaupama@icloud.com>
2018-12-11 13:08:40 +1300

    added new get route function for all records

--- client/actions/records.js
+++ client/actions/records.js
@@ -1,44 +1,53 @@
 import request from 'superagent'
 import moment from 'moment'
 
-export function addRecordSuccess (records) {
+export function addRecordSuccess(records) {
   return {
     type: 'ADD_RECORD_SUCCESS',
     records
   }
 }
 
-export function getRecordPending () {
+export function getRecordPending() {
   return {
     type: 'GET_RECORD_PENDING'
   }
 }
 
-export function getRecordError (message) {
+export function getRecordError(message) {
   return {
     type: 'GET_RECORD_ERROR',
     message
   }
 }
 
-export function addActivity (userId, cardData) {
+export function getRecords(userId, date) {
   return dispatch => {
     dispatch(getRecordPending())
+    request
+      .get(`/api/v1/records/cards/${userId}/${date}`)
+      .then(res => dispatch(getRecordsSuccess(res.body.records)))
+      .catch(err => dispatch(getRecordsError(err.message)))
+  }
+}
 
+export function addActivity(userId, cardData) {
+  return dispatch => {
+    dispatch(getRecordPending())
     return request
       .post('/api/v1/records')
-      .send({userId, date: moment().format('YYYY-MM-DD'), cardData})
+      .send({ userId, date: moment().format('YYYY-MM-DD'), cardData })
       .then(res => dispatch(addRecordSuccess(res.body.records)))
       .catch(err => dispatch(getRecordError(err.message)))
   }
 }
 
-export function addLog (userId, cardData) {
+export function addLog(userId, cardData) {
   return dispatch => {
     dispatch(getRecordPending())
     return request
       .post('/api/v1/records') // we may need a new api?
-      .send({userId, date: moment().format('YYYY-MM-DD'), cardData})
+      .send({ userId, date: moment().format('YYYY-MM-DD'), cardData })
       .then(res => {
         dispatch(addRecordSuccess(res.body.records))
       })
