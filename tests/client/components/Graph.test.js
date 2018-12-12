de7a2a25eaa4cc3a0d3fcb57d8bcf363e950516b
Alina <alna.kireeva88@gmail.com>
2018-12-12 14:23:32 +1300

    Created Graph.test.js, tested first part

---
+++ tests/client/components/Graph.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import ShallowRenderer from 'react-test-renderer/shallow'
+import {mount} from 'enzyme'
+// import Adapter from 'enzyme-adapter-react-16'
+
+import Graph from '../../../client/components/Graph'
+
+test('Initial test', () => {
+  expect(1).toBe(1)
+})
+
+test('<Graph /> in Search', () => {
+  const expected = ''
+  const wrapper = mount(<Graph />)
+  expect(wrapper.text()).toMatch(expected)
+})
